85f260df063b6312f778252be7e50c078462f5ee
Oliver Lundin <oliverlundin@olivers-mbp.students.netlogon.liu.se>
2023-12-11 15:49:13 +0100

    leveled amount of data

--- comparison.py
+++ comparison.py
@@ -1,136 +1,137 @@
-
+from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from matplotlib.colors import ListedColormap
-
-from sklearn.datasets import make_circles, make_classification, make_moons
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
-from sklearn.inspection import DecisionBoundaryDisplay
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+# Load data from CSV
+data = pd.read_csv('combined.csv')
+
+# Assuming 'feature1' and 'feature2' are your feature columns and 'target' is the target column
+X = data[['content']].values
+y = data['label'].values
 
 names = [
     "Nearest Neighbors",
     "Linear SVM",
     "RBF SVM",
-    "Gaussian Process",
     "Decision Tree",
     "Random Forest",
     "Neural Net",
     "AdaBoost",
-    "Naive Bayes",
-    "QDA",
 ]
 
 classifiers = [
     KNeighborsClassifier(3),
     SVC(kernel="linear", C=0.025, random_state=42),
     SVC(gamma=2, C=1, random_state=42),
-    GaussianProcessClassifier(1.0 * RBF(1.0), random_state=42),
     DecisionTreeClassifier(max_depth=5, random_state=42),
     RandomForestClassifier(
         max_depth=5, n_estimators=10, max_features=1, random_state=42
     ),
     MLPClassifier(alpha=1, max_iter=1000, random_state=42),
     AdaBoostClassifier(random_state=42),
-    GaussianNB(),
-    QuadraticDiscriminantAnalysis(),
-]
-
-X, y = make_classification(
-    n_features=2, n_redundant=0, n_informative=2, random_state=1, n_clusters_per_class=1
-)
-rng = np.random.RandomState(2)
-X += 2 * rng.uniform(size=X.shape)
-linearly_separable = (X, y)
-
-datasets = [
-    make_moons(noise=0.3, random_state=0),
-    make_circles(noise=0.2, factor=0.5, random_state=1),
-    linearly_separable,
 ]
 
 figure = plt.figure(figsize=(27, 9))
 i = 1
-# iterate over datasets
-for ds_cnt, ds in enumerate(datasets):
-    # preprocess dataset, split into training and test part
-    X, y = ds
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.4, random_state=42
-    )
 
-    x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
-    y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
+# Initialize the TF-IDF vectorizer
+# You can adjust max_features as needed
+tfidf_vectorizer = TfidfVectorizer(max_features=1500)
 
-    # just plot the dataset first
-    cm = plt.cm.RdBu
-    cm_bright = ListedColormap(["#FF0000", "#0000FF"])
-    ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
-    if ds_cnt == 0:
-        ax.set_title("Input data")
-    # Plot the training points
-    ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train,
+# Transform the text data into TF-IDF features
+X_tfidf = tfidf_vectorizer.fit_transform(X)
+
+# preprocess dataset, split into training and test part
+X_train, X_test, y_train, y_test = train_test_split(
+    X_tfidf, y, test_size=0.2, random_state=40)
+
+# Apply PCA to reduce the TF-IDF features to 2 dimensions
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X_tfidf.toarray())
+
+x_min, x_max = X_pca[:, 0].min() - 0.5, X_pca[:, 0].max() + 0.5
+y_min, y_max = X_pca[:, 1].min() - 0.5, X_pca[:, 1].max() + 0.5
+
+
+cm = plt.cm.RdBu
+cm_bright = ListedColormap(["#FF0000", "#0000FF"])
+
+plt.scatter(X_pca[:, 0], X_pca[:, 1], c=y_train,
+            cmap=cm_bright, edgecolors="k", label='Training Data')
+
+plt.scatter(X_pca[:, 0], X_pca[:, 1], c=y_test,
+            cmap=cm_bright, alpha=0.6, edgecolors="k", label='Testing Data')
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.xticks(())
+plt.yticks(())
+plt.legend()
+
+# iterate over classifiers
+for name, clf in zip(names, classifiers):
+    plt.figure(figsize=(6, 4))
+    clf = make_pipeline(StandardScaler(), clf)
+    clf.fit(X_train, y_train)
+    score = clf.score(X_test, y_test)
+
+    # Create scatter plot for training data
+    plt.scatter(X_pca[:, 0], X_pca[:, 1], c=y_train,
+                cmap=cm_bright, edgecolors="k", label='Training Data')
+
+    # Create scatter plot for testing data
+    plt.scatter(X_pca[:, 0], X_pca[:, 1], c=y_test,
+                cmap=cm_bright, alpha=0.6, edgecolors="k", label='Testing Data')
+
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+    plt.xticks(())
+    plt.yticks(())
+    plt.title(f"{name} - Accuracy: {score:.2f}")
+    plt.legend()
+
+plt.tight_layout()
+plt.show()
+
+# iterate over classifiers
+for name, clf in zip(names, classifiers):
+    ax = plt.subplot(1, len(classifiers) + 1, i + 1)
+
+    clf = make_pipeline(StandardScaler(), clf)
+    clf.fit(X_train, y_train)
+    score = clf.score(X_test, y_test)
+
+    # DecisionBoundaryDisplay is not compatible with all classifiers, so let's use scatter plots instead
+    ax.scatter(X_pca[:, 0], X_pca[:, 1], c=y_train,
                cmap=cm_bright, edgecolors="k")
-    # Plot the testing points
-    ax.scatter(
-        X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6, edgecolors="k"
-    )
+    ax.scatter(X_pca[:, 0], X_pca[:, 1], c=y_test,
+               cmap=cm_bright, alpha=0.6, edgecolors="k")
+
     ax.set_xlim(x_min, x_max)
     ax.set_ylim(y_min, y_max)
     ax.set_xticks(())
     ax.set_yticks(())
+    ax.set_title(name)
+    ax.text(
+        x_max - 0.3,
+        y_min + 0.3,
+        ("%.2f" % score).lstrip("0"),
+        size=15,
+        horizontalalignment="right",
+    )
     i += 1
-
-    # iterate over classifiers
-    for name, clf in zip(names, classifiers):
-        ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
-
-        clf = make_pipeline(StandardScaler(), clf)
-        clf.fit(X_train, y_train)
-        score = clf.score(X_test, y_test)
-        DecisionBoundaryDisplay.from_estimator(
-            clf, X, cmap=cm, alpha=0.8, ax=ax, eps=0.5
-        )
-
-        # Plot the training points
-        ax.scatter(
-            X_train[:, 0], X_train[:,
-                                   1], c=y_train, cmap=cm_bright, edgecolors="k"
-        )
-        # Plot the testing points
-        ax.scatter(
-            X_test[:, 0],
-            X_test[:, 1],
-            c=y_test,
-            cmap=cm_bright,
-            edgecolors="k",
-            alpha=0.6,
-        )
-
-        ax.set_xlim(x_min, x_max)
-        ax.set_ylim(y_min, y_max)
-        ax.set_xticks(())
-        ax.set_yticks(())
-        if ds_cnt == 0:
-            ax.set_title(name)
-        ax.text(
-            x_max - 0.3,
-            y_min + 0.3,
-            ("%.2f" % score).lstrip("0"),
-            size=15,
-            horizontalalignment="right",
-        )
-        i += 1
 
 plt.tight_layout()
 plt.show()
