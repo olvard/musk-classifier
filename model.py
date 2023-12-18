e320dfed97d5d827e14220120e74d46f0bb1eb1f
Oliver Lundin <79059027+olvard@users.noreply.github.com>
2023-12-18 11:23:59 +0100

    cleaned

--- model.py
+++ model.py
@@ -11,61 +11,49 @@ from sklearn.naive_bayes import MultinomialNB
 # Load the data
 combined_tweets = pd.read_csv('combined.csv')
 
-# Display the first few rows of the dataset
 print(combined_tweets.head())
 
-# Fill missing values in the 'content' column and convert non-string types to strings
+# Fill missing values in the 'content' column
 combined_tweets['content'] = combined_tweets['content'].fillna('').astype(str)
 
 # Split data into features (tweets) and labels
 X = combined_tweets['content']
 y = combined_tweets['label']
 
-# Initialize the TF-IDF vectorizer
-# You can adjust max_features as needed
+# TF-IDF
 tfidf_vectorizer = TfidfVectorizer(max_features=3500)
 
-# Transform the text data into TF-IDF features
 X_tfidf = tfidf_vectorizer.fit_transform(X)
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(
     X_tfidf, y, test_size=0.2, random_state=42)
 
-# Define the parameters grid to search
+# Grid Search
 param_grid = {
-    'alpha': [0.1, 0.5, 1.0, 1.5, 2.0],  # Add more values for alpha if needed
-    # Other parameters can also be added and tuned as needed
+    'alpha': [0.1, 0.5, 1.0, 1.5, 2.0],
 }
 
 classifier = MultinomialNB()
-
-# Initialize the GridSearchCV
 grid_search = GridSearchCV(classifier, param_grid, cv=5, n_jobs=-1)
-
-# Fit the data to find the best parameters
 grid_search.fit(X_train, y_train)
 
 best_params = grid_search.best_params_
 print("Best Parameters:", best_params)
 
-# Use the best parameters to create the final classifier
 best_classifier = MultinomialNB(alpha=best_params['alpha'])
 
-# Train the classifier using the best parameters
 best_classifier.fit(X_train, y_train)
 
-# Predict on the test set
 y_pred = best_classifier.predict(X_test)
 
 # Calculate accuracy
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy:.2f}")
 
-# Generate classification report
 print(classification_report(y_test, y_pred))
 
-# Visualize confusion matrix after tuning
+# confusion matrix after tuning
 conf_matrix_tuned = confusion_matrix(y_test, y_pred)
 plt.figure(figsize=(8, 6))
 sns.heatmap(conf_matrix_tuned, annot=True, fmt='d', cmap='Blues',
--- topwords.py
+++ topwords.py
@@ -4,25 +4,19 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 # Load the data
 combined_tweets = pd.read_csv('combined.csv')
 
-# Filter the dataset to include only entries labeled as 'Elon'
 elon_tweets = combined_tweets[combined_tweets['label'] == 'Elon Musk']
 
 print(elon_tweets.head())
 
-# Fill missing values in the 'content' column and convert non-string types to strings
 elon_tweets['content'] = elon_tweets['content'].fillna('').astype(str)
 
-# Split data into features (tweets) and labels for Elon tweets
 X_elon = elon_tweets['content']
 y_elon = elon_tweets['label']
 
-# Initialize the TF-IDF vectorizer for Elon's tweets
 tfidf_vectorizer_elon = TfidfVectorizer(max_features=3500)
 
-# Transform the text data into TF-IDF features for Elon's tweets
 X_tfidf_elon = tfidf_vectorizer_elon.fit_transform(X_elon)
 
-# Get the feature names (words) from the TF-IDF vectorizer for Elon's tweets
 feature_names_elon = tfidf_vectorizer_elon.get_feature_names_out()
 
 # Sum up the occurrences of each word across all Elon's documents
@@ -37,7 +31,7 @@ for word, freq in zip(feature_names_elon, word_frequencies_elon.tolist()[0]):
 sorted_word_frequencies_elon = sorted(
     word_frequency_dict_elon.items(), key=lambda x: x[1], reverse=True)
 
-# Display the top five most used words for Elon's tweets
+# Display the top 20 most used words for Elon's tweets
 top_five_words_elon = sorted_word_frequencies_elon[:20]
 for word, frequency in top_five_words_elon:
     print(f"Word: {word}, Frequency: {frequency}")
